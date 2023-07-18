182868cd73c40c87fdec155bc1a0eb17c3170f48
JakubKorytko <jakub.korytko@o2.pl>
2023-07-18 16:02:16 +0200

    Add finished projects

---
+++ Projekty/Projekt 1.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <math.h>
+
+// +---------------+
+// | Projekt nr 17 |
+// +---------------+
+
+double szereg(double x)
+{
+    double s = 1, w = 1;
+    for (int i = 1; i <= 100; i++)
+    {
+        w *= -x * x / (2 * i * (2 * i - 1));
+        s += w;
+    }
+    return s;
+}
+
+int main()
+{
+    int c;
+    char table[31] = " ----------------------------\n";
+    double a, b, k, x;
+
+    printf("Przedzial to ( |x| < nieskonczonosc )\n");
+
+    printf("Podaj poczatek przedzialu: ");
+    scanf("%lf", &a);
+
+    printf("Podaj koniec przedzialu: ");
+    scanf("%lf", &b);
+
+    printf("Podaj liczbe podprzedzialow: ");
+    scanf("%d", &c);
+
+    k = (b - a) / c;
+
+    printf("Krok = %.2lf\n\n", k);
+    printf("%s", table);
+    printf("       x  szereg(x) cos(x)\n");
+    printf("%s", table);
+
+    for (x = a; x <= b; x += k)
+    {
+        printf("%10.2lf %7.4lf %7.4lf\n", x, szereg(x), cos(x));
+    }
+
+    printf("%s", table);
+
+    return 0;
+}
---
+++ Projekty/Projekt 2.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <math.h>
+
+// +-----------+
+// | Projekt L |
+// +-----------+
+
+int sum(int x)
+{
+    int s = 0;
+    int l = x;
+
+    while (l > 0)
+    {
+        int i = 1, p = 0;
+
+        while (i * 10 < l)
+        {
+            i *= 10;
+        }
+
+        p = floor(l / i);
+
+        s += p;
+
+        l -= p * i;
+    }
+
+    return s;
+}
+
+int main()
+{
+
+    int koniec = 0, k = 0, l = 1, c = 0;
+
+    printf("Podaj wartosc koncowa: ");
+    scanf("%d", &koniec);
+
+    do
+    {
+        printf("Podaj %d element: ", l);
+        scanf("%d", &k);
+        if (k < koniec && k >= 10 && k < 1000 && sum(k) % 2 != 0)
+        {
+            c++;
+        }
+        l += 1;
+    } while (k < koniec);
+
+    printf("Liczba elementow spelniajacych warunek: %d", c);
+
+    return 0;
+}
